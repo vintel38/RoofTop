45efd8e69dc633b1b0d514f27dc8fe2912ea18ea
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-08-03 11:06:46 +0200

    understand balloon

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -73,10 +73,10 @@ def apply_mask(image, mask, color, alpha=0.5):
     """Apply the given mask to the image.
     """
     for c in range(3):
-        image[:, :, c] = np.where(mask == 1,
-                                  image[:, :, c] *
-                                  (1 - alpha) + alpha * color[c] * 255,
-                                  image[:, :, c])
+        #print(mask.shape)
+        #print(image[:, :, c].shape)
+        #print(image[:, :, c])
+        image[:, :, c] = np.where(mask == 1,image[:, :, c] *(1 - alpha) + alpha * color[c] * 255,image[:, :, c])
     return image
 
 
@@ -165,7 +165,83 @@ def display_instances(image, boxes, masks, class_ids, class_names,
     ax.imshow(masked_image.astype(np.uint8))
     if auto_show:
         plt.show()
+        
+# Programmer Sought 
 
+def get_display_instances_pic(image, boxes, masks, class_ids, class_number, ax=None,
+    class_names=None,scores=None, show_mask=True, show_bbox=True,colors=None):
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_number:a int about  How many class you have
+    ax: used for save the pic
+    class_names(optional): list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    colors: (optional) An array or colors to use with each object
+    """
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances detected *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
+ 
+    if not ax:
+        _, ax = plt.subplots(1, figsize=(16,16))
+        auto_show = True
+ 
+    # Generate random colors
+    colors =random_colors(class_number)  if colors==None else colors
+ 
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+ 
+    masked_image = image.astype(np.uint32).copy()
+ 
+    for i in range(N):
+        class_id = class_ids[i]
+        color = colors[class_id]
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+ 
+        # Label
+        if class_names == None:
+            label =  str(class_id) 
+        else:
+            label = class_names[class_id]
+        score = scores[i] if scores is not None else None
+        caption = "cls: {}  scr:{:.3f}".format(label, score) if score else "cls: {}".format(label)
+ 
+        ax.text(x1, y1 + 8, caption, color='red', size=11, backgroundcolor="none")
+ 
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+ 
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros((mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
 
 def display_differences(image,
                         gt_box, gt_class_id, gt_mask,
