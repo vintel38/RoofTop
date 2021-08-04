40686738b8eb85f496839d772b33803844f127d2
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-08-04 08:30:06 +0200

    more update23

--- samples/balloon/balloon_labelme.py
+++ samples/balloon/balloon_labelme.py
@@ -58,11 +58,11 @@ class BalloonConfig(Config):
     
 #############################################################
  
-# class InferenceConfig(ModelConfig):
+class InferenceConfig(Config):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-    # GPU_COUNT = 1
-    # IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
  
 ############################################################
 #  Dataset (My labelme dataset loader)
