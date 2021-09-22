2dda05465ca0dc4577f39248d39d7d9033024ce6
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-09-22 11:42:34 +0200

    automate GIS coord transfo

--- doc/bin/cvtTIF2PNG.py
+++ doc/bin/cvtTIF2PNG.py
@@ -1,71 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 11 16:50:09 2020
-Modified on Mon Jul 19 15:16:23 2021
+Created on Tue Jul 20 10:50:09 2021
 
-@author: MUSTAFAAKTAS, Vintel38
+@author: Vintel38
+
+based on Programmer Sought webpage 
+https://www.programmersought.com/article/48811569657/
+
+Le sens des slashs semble être important dans le bon fonctionnement du système. 
+
 """
 
+
 from osgeo import gdal
-import numpy as np
 import os
-import subprocess
 
-def converter(inputRaster, outputRaster, outputPixType='Byte', outputFormat='png', percentiles=[2, 98]):
-    '''
-    Convert 16bit image to 8bit
-    Source: Medium.com, 'Creating Training Datasets for the SpaceNet Road Detection and Routing Challenge' by Adam Van Etten and Jake Shermeyer
-    '''
-    print(inputRaster)
-    srcRaster = gdal.Open(inputRaster)
-    cmd = ['gdal_translate', '-ot', outputPixType, '-of', 
-           outputFormat]
+def converter(file_path, output, extension):
+    # file_path="D:/work/python/Tif_to_png/ a_image.tif"
+    ds=gdal.Open(file_path)
+    print(file_path)
+    driver=gdal.GetDriverByName(extension)
+    # dst_ds = driver.CreateCopy('D:/work/python/Tif_to_png/example.png', ds)
+    dst_ds = driver.CreateCopy(output, ds)
+    dst_ds = None
+    src_ds = None
     
-    # iterate through bands
-    for bandId in range(srcRaster.RasterCount):
-        bandId = bandId+1
-        band = srcRaster.GetRasterBand(bandId)
-
-        bmin = band.GetMinimum()        
-        bmax = band.GetMaximum()
-        # if not exist minimum and maximum values
-        if bmin is None or bmax is None:
-            (bmin, bmax) = band.ComputeRasterMinMax(1)
-        # else, rescale
-        band_arr_tmp = band.ReadAsArray()
-        bmin = np.percentile(band_arr_tmp.flatten(), 
-                             percentiles[0])
-        bmax= np.percentile(band_arr_tmp.flatten(), 
-                            percentiles[1])
-
-        cmd.append('-scale_{}'.format(bandId))
-        cmd.append('{}'.format(bmin))
-        cmd.append('{}'.format(bmax))
-        cmd.append('{}'.format(0))
-        cmd.append('{}'.format(255))
-    cmd.append(inputRaster)
-    cmd.append(outputRaster)
-    print("Conversin command:", cmd)
-    subprocess.call(cmd)
+dir_path = "C:/Users/VArri/Documents/Rooftop/dataset/dataset/dataset/colab/val"
+# output_path = "C:/Users/VArri/Documents/Rooftop/dataset/dataset/dataset/colab/austin1.png"
+extension = 'PNG'
+# converter(file_path, output_path, extension)
     
-if __name__ == '__main__':
-    import argparse
+# if __name__ == '__main__':
+    # import argparse
     
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Convert TIF files to PNG format')
-    parser.add_argument('--dataset', required=True,
-                        metavar="/path/to/dataset/",
-                        help='Directory of your dataset')
-    parser.add_argument('--output_path', required=True,
-                        metavar="/path/to/output/",
-                        help='Directory of output')
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser(description='Convert TIF files to PNG format')
+    # parser.add_argument('--dataset', required=True,
+                        # metavar="/path/to/dataset/",
+                        # help='Directory of your dataset')
+    # parser.add_argument('--output_path', required=True,
+                        # metavar="/path/to/output/",
+                        # help='Directory of output')
+    # parser.add_argument('--extension', 
+                        # metavar="type",
+                        # help='Extension of the output')
+    # args = parser.parse_args()
     
     # path = "D:/DATASET/SpaceNet/Train/AOI_2_Vegas_Train/RGB-PanSharpen/"
-    files = os.listdir(args.dataset)
-
-    for file in files:
-        resimPath = args.dataset+'/'+file
-        # dstPath   = "D:/DATASET/SpaceNet/Train/AOI_2_Vegas_Train/RGB-PanSharpen-NEW/"
-        dstPath   = args.output_path+'/'+file
-        converter(resimPath,dstPath)
+    
+files = os.listdir(dir_path)
+for file in files:
+    a, b = os.path.splitext(file) 
+    print((a,b))
+    if b != '.tif':
+        continue
+    print('ok')
+    resimPath = dir_path + '/' + file
+    dstPath   = dir_path+ '/' + a + '.png'
+    converter(resimPath,dstPath, extension)
---
+++ samples/darkroof/post_gis.py
@@ -0,0 +1,66 @@
+# script to post process the data emitted from MaskRCNN for the detections
+# takes as input the r variables stored in elt file 
+
+# import 
+import os 
+import numpy 
+from osgeo import gdal,ogr,osr
+
+
+def GetExtent(ds):
+    """ Return list of corner coordinates from a gdal Dataset """
+    xmin, xpixel, _, ymax, _, ypixel = ds.GetGeoTransform()
+    width, height = ds.RasterXSize, ds.RasterYSize
+    xmax = xmin + width * xpixel
+    ymin = ymax + height * ypixel
+    return xmin, xmax, ymin, ymax, xpixel, ypixel
+    
+    
+def pointing(coord, img_coord, res):
+    """ajoute aux img_coord métriques, la distance de chaque détection coord en nombre de pixels"""
+    xmin, xmax, ymin, ymax = img_coord
+    x_coord, y_coord = coord
+    x_c = xmin + res[0]*x_coord
+    y_c = ymax - res[1]*y_coord
+    return x_c, y_c 
+    
+
+def ReprojectCoords(coords,src_srs,tgt_srs):
+    """ Reproject a list of x,y coordinates. """
+    # https://gis.stackexchange.com/questions/57834/how-to-get-raster-corner-coordinates-using-python-gdal-bindings
+    trans_coords=[]
+    transform = osr.CoordinateTransformation( src_srs, tgt_srs)
+    for x,y in coords:
+        x,y,z = transform.TransformPoint(x,y)
+        trans_coords.append([x,y])
+    return trans_coords
+
+
+def post_process(img_path, elt):
+    """chargement et extraction des variables bbox masks class et scores du fichier elt """
+    assert elt.endswith('.npy')
+    data = np.load(elt, allow_pickle=True)
+    
+    bbox=np.array(dict(data.item(0))['rois'])
+    masks=np.array(dict(data.item(0))['masks'])
+    classes=np.array(dict(data.item(0))['class_ids'])
+    scores=np.array(dict(data.item(0))['scores'])
+    
+    raster = gdal.Open(img_path)
+    ext = GetExtent(raster)
+    
+    src_srs=osr.SpatialReference()
+    src_srs.ImportFromWkt(raster.GetProjection())
+    #tgt_srs=osr.SpatialReference()
+    #tgt_srs.ImportFromEPSG(4326)
+    tgt_srs = src_srs.CloneGeogCS()
+    
+    pt=[]
+    for i in range(masks.shape[-1]):
+        y_center, x_center = np.argwhere(masks[:,:,i]==1).sum(0)/(masks[:,:,i] == 1).sum()
+        point = pointing((x_center, y_center), ext[0:4], ext[4:6])
+        print(point)
+        pt.append(point)
+        
+    reproj = ReprojectCoords(pt, src_srs, tgt_srs)
+    return reproj
