a451f3ee00350c51d52ea9f076f454691d5c44ac
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-11-19 23:15:06 +0100

    load INRIA dataset changes

--- samples/darkroof/darkroof_labelme.py
+++ samples/darkroof/darkroof_labelme.py
@@ -10,6 +10,7 @@ import datetime
 import numpy as np
 import skimage.draw
 import imgaug
+import cv2
  
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../..")
@@ -206,9 +207,90 @@ class DarkRoofDataset(utils.Dataset):
  
         for labelid, labelname in enumerate(labelslist):
             self.add_class(source,labelid,labelname)
+            
+    def load_INRIA(self, dataset_dir, subset):
+        """
+        Load a subset of the INRIA dataset. Semantic segmentation with only one class. 
+        source: coustomed source id, exp: load data from coco, than set it "coco",
+                it is useful when you ues different dataset for one trainning.(TODO)
+                see the prepare func in utils model for details
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val
+        """
+        # Train or validation dataset?
+        assert subset in ["train", "test"]
+        dataset_dir = os.path.join(dataset_dir, subset, 'images')
+ 
+        filenames = os.listdir(dataset_dir)
+        # jsonfiles,annotations=[],[]
+        # for filename in filenames:
+            # if filename.endswith(".json"):
+                # jsonfiles.append(filename)
+                # annotation = json.load(open(os.path.join(dataset_dir,filename)))
+                # # Insure this picture is in this dataset
+                # imagename = annotation['imagePath']
+                # if not os.path.isfile(os.path.join(dataset_dir,imagename)):
+                    # continue
+                # if len(annotation["shapes"]) == 0:
+                    # continue
+                # # you can filter what you don't want to load
+                # annotations.append(annotation)
+               
+        # print("In {source} {subset} dataset we have {number:d} annotation files."
+            # .format(source=source, subset=subset,number=len(jsonfiles)))
+        # print("In {source} {subset} dataset we have {number:d} valid annotations."
+            # .format(source=source, subset=subset,number=len(annotations)))
+        # annotation contain the all annotation
+            
+        # Add images and get all classes in annotation files
+        # typically, after labelme's annotation, all same class item have a same name
+        # this need us to annotate like all "ball" in picture named "ball"
+        # not "ball_1" "ball_2" ...
+        # we also can figure out which "ball" it is refer to.
+        labelslist = ['building']
+        for file_name in filenames:
+        
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. These are stores in the
+            # shape_attributes (see json format above)
+            # shapes = [] 
+            classids = []
+        
+            im = cv2.imread(os.path.join(dataset_dir,file_name))
+            imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            
+            _, thresh = cv2.threshold(imgray, 127, 255, 0)
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            
+            # for _ in contours:
+                # # first we get the shape classid
+                # label = 'building'
+                # #if labelslist.count(label) == 0:
+                # #    labelslist.append(label)
+                # classids.append(labelslist.index(label)+1)
+                # #shapes.append(shape["points"])
+            classids = [1 for _ in range(len(contours))]
+            
+            # load_mask() needs the image size to convert polygons to masks.
+            width = 5000
+            height = 5000
+            self.add_image(
+                source,
+                image_id=file_name,  # use file name as a unique image id
+                path=os.path.join(dataset_dir,file_name),
+                width=width, height=height,
+                shapes=contours, classids=classids)
+            ### FUNCTION AVAILABLE IN PACKAGE UTILS.PY
+ 
+        print("In {source} {subset} dataset we have {number:d} class item"
+            .format(source=source, subset=subset,number=len(labelslist)))
+ 
+        for labelid, labelname in enumerate(labelslist):
+            self.add_class(source,labelid,labelname)
  
     def load_mask(self,image_id):
         """
+        LOAD_MASK FUNCTION USED IN TRAIN FUNCTION
         Generate instance masks for an image.
        Returns:
         masks: A bool array of shape [height, width, instance count] with one mask per instance.
@@ -234,6 +316,35 @@ class DarkRoofDataset(utils.Dataset):
         # Return mask, and array of class IDs of each instance. Since we have
         # one class ID only, we return an array of 1s
         return masks_np, classids_np
+        
+    def load_mask_INRIA(self,image_id):
+        """
+        LOAD_MASK FUNCTION USED IN TRAIN FUNCTION
+        Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not the source dataset you want, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != source:
+            return super(self.__class__, self).load_mask(image_id)
+ 
+        # Convert shapes to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        mask = np.zeros([info["height"], info["width"], len(info["shapes"])], dtype=np.uint8)
+        #printsx,printsy=zip(*points)
+        for idx, points in enumerate(info["shapes"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            pointsx,pointsy = zip(*points)
+            rr, cc = skimage.draw.polygon(pointsx, pointsy)
+            mask[rr, cc, idx] = 1
+        masks_np = mask.astype(np.bool)
+        classids_np = np.array(image_info["classids"]).astype(np.int32)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return masks_np, classids_np
  
     def image_reference(self,image_id):
         """Return the path of the image."""
@@ -251,6 +362,7 @@ def train(dataset_train, dataset_val, model, augmentation):
  
     # Validation dataset
     dataset_val.prepare()
+    ### FUNCTIONS PREPARE AVAILABLE IN PACKAGE UTILS.PY 
  
     # *** This training schedule is an example. Update to your needs ***
     print("Training network heads")
@@ -335,7 +447,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Train Mask R-CNN to detect balloons.')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="'train' or 'test' or 'inference'")
+                        help="'train' or 'test' or 'inference' or 'train_INRIA'")
     parser.add_argument('--dataset', required=False,
                         metavar="/path/to/dataset/",
                         help='Directory of your dataset')
@@ -357,14 +469,14 @@ if __name__ == '__main__':
                         help="Class number of your detector.")
     parser.add_argument('--DA', required=False,
                         metavar="Data Augmentation?",
-                        help="Do you want imgau on the training phase?")
+                        help="Do you want imgaug on the training phase?")
     # https://stackoverflow.com/questions/23566970/using-argparse-to-create-output-file
     parser.add_argument("-o", "--output", required=False,
                         help="Directs the output to a name of your choice")
     args = parser.parse_args()
  
     # Validate arguments
-    if args.command == "train":
+    if args.command == "train" or args.command == "train_INRIA":
         assert args.dataset, "Argument --dataset is required for training"
     elif args.command == "test":
         assert args.image or args.video or args.classnum, \
@@ -385,6 +497,12 @@ if __name__ == '__main__':
         dataset_train.load_darkroof(args.dataset,"train")
         dataset_val.load_darkroof(args.dataset,"val")
         config.NUM_CLASSES = len(dataset_train.class_info)
+    elif args.command == "train_INRIA":
+        config = DarkRoofConfig()
+        dataset_train, dataset_val = DarkRoofDataset(), DarkRoofDataset()
+        dataset_train.load_INRIA(args.dataset,"train")
+        dataset_val.load_INRIA(args.dataset,"val")
+        config.NUM_CLASSES = len(dataset_train.class_info)
     elif args.command == "test":
         config = InferenceConfig()
         config.NUM_CLASSES = int(args.classnum)+1 # add backgrouond
@@ -401,7 +519,7 @@ if __name__ == '__main__':
     config.display()
  
     # Create model
-    if args.command == "train":
+    if args.command == "train" or args.command == "train_INRIA":
         model = modellib.MaskRCNN(mode="training", config=config,model_dir=args.logs)
     else:
         model = modellib.MaskRCNN(mode="inference", config=config, model_dir=args.logs)
@@ -425,6 +543,28 @@ if __name__ == '__main__':
     print("Loading weights ", weights_path)
     
     if args.command == "train":
+        if args.weights.lower() == "coco":
+            # Exclude the last layers because they require a matching
+            # number of classes if we change the backbone?
+            model.load_weights(weights_path, by_name=True, exclude=[
+                "mrcnn_class_logits", "mrcnn_bbox_fc",
+                "mrcnn_bbox", "mrcnn_mask"])
+            print("Loading weights finished")
+        else:
+            model.load_weights(weights_path, by_name=True)
+            
+        # DA Data Augmentation
+        if args.DA:        
+            augmentation = imgaug.augmenters.Sometimes(0.5, [
+                imgaug.augmenters.Fliplr(0.5),
+                imgaug.augmenters.GaussianBlur(sigma=(0.0, 5.0))
+                ])
+        
+        # Train
+        print("Start Training !")
+        train(dataset_train, dataset_val, model, augmentation)
+        
+    elif args.command == "train_INRIA":
         if args.weights.lower() == "coco":
             # Exclude the last layers because they require a matching
             # number of classes if we change the backbone?
