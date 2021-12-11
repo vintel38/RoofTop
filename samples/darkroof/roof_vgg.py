d7bff3ed5a6254097564750a09bf9b4e4e451442
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-12-11 08:27:05 +0100

    test INRIA weights update import

--- samples/darkroof/roof_vgg.py
+++ samples/darkroof/roof_vgg.py
@@ -40,7 +40,9 @@ ROOT_DIR = os.path.abspath("../../")
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
 from mrcnn.config import Config
-from mrcnn import model as modellib, utils
+from mrcnn import model as modellib
+from mrcnn import utils
+from mrcnn import visualize
 
 # Path to trained weights file
 COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
@@ -207,7 +209,7 @@ def train(model, epochs=30):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs,
+                epochs=epochs,
                 layers='heads')
                 
                 
