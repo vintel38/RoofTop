b870f33de4824b75e414be16cece345ab9ab152c
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-12-12 10:37:39 +0100

    submission boxes extraction

--- samples/cells/cells_vgg.py
+++ samples/cells/cells_vgg.py
@@ -33,6 +33,7 @@ import json
 import datetime
 import numpy as np
 import skimage.draw
+import csv
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../../")
@@ -251,79 +252,7 @@ def test(model, image_path = None, video_path=None, savedfile=None):
     elif video_path:
         pass
     print("Saved to ", os.getcwd() , file_name)
-
-
-# def color_splash(image, mask):
-    # """Apply color splash effect.
-    # image: RGB image [height, width, 3]
-    # mask: instance segmentation mask [height, width, instance count]
-
-    # Returns result image.
-    # """
-    # # Make a grayscale copy of the image. The grayscale copy still
-    # # has 3 RGB channels, though.
-    # gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-    # # Copy color pixels from the original color image where mask is set
-    # if mask.shape[-1] > 0:
-        # # We're treating all instances as one, so collapse the mask into one layer
-        # mask = (np.sum(mask, -1, keepdims=True) >= 1)
-        # splash = np.where(mask, image, gray).astype(np.uint8)
-    # else:
-        # splash = gray.astype(np.uint8)
-    # return splash
-
-
-# def detect_and_color_splash(model, image_path=None, video_path=None):
-    # assert image_path or video_path
-
-    # # Image or video?
-    # if image_path:
-        # # Run model detection and generate the color splash effect
-        # print("Running on {}".format(args.image))
-        # # Read image
-        # image = skimage.io.imread(args.image)
-        # # Detect objects
-        # r = model.detect([image], verbose=1)[0]
-        # # Color splash
-        # splash = color_splash(image, r['masks'])
-        # # Save output
-        # file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-        # skimage.io.imsave(file_name, splash)
-    # elif video_path:
-        # import cv2
-        # # Video capture
-        # vcapture = cv2.VideoCapture(video_path)
-        # width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        # height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # fps = vcapture.get(cv2.CAP_PROP_FPS)
-
-        # # Define codec and create video writer
-        # file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        # vwriter = cv2.VideoWriter(file_name,
-                                  # cv2.VideoWriter_fourcc(*'MJPG'),
-                                  # fps, (width, height))
-
-        # count = 0
-        # success = True
-        # while success:
-            # print("frame: ", count)
-            # # Read next image
-            # success, image = vcapture.read()
-            # if success:
-                # # OpenCV returns images as BGR, convert to RGB
-                # image = image[..., ::-1]
-                # # Detect objects
-                # r = model.detect([image], verbose=0)[0]
-                # # Color splash
-                # splash = color_splash(image, r['masks'])
-                # # RGB -> BGR to save image to video
-                # splash = splash[..., ::-1]
-                # # Add image to video writer
-                # vwriter.write(splash)
-                # count += 1
-        # vwriter.release()
-    # print("Saved to ", file_name)
-
+    
 
 ############################################################
 #  Training
@@ -360,6 +289,9 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', required=False,
                         metavar="Number of epochs for the training phase",
                         help="Nb of training phases")
+    # https://stackoverflow.com/questions/23566970/using-argparse-to-create-output-file
+    parser.add_argument("-o", "--output", required=False,
+                        help="Output folder of your choice")
     args = parser.parse_args()
 
     # Validate arguments
@@ -430,5 +362,19 @@ if __name__ == '__main__':
                 model.load_weights(os.path.join(args.weights,filename),by_name=True)
                 savedfile_name = os.path.splitext(filename)[0] + ".jpg"
                 test(model, image_path=args.image,video_path=args.video, savedfile=savedfile_name)
+    elif args.command == "submission":
+        filenames = os.listdir(args.image)
+        print("Load weights from {filename} ".format(filename=args.weights))
+        model.load_weights(os.path.join(args.weights),by_name=True)
+        for filename in filenames:
+            name, _ = filename.split('.')
+            image = skimage.io.imread(os.path.join(args.image, filename))
+            # Detect objects
+            r = model.detect([image], verbose=1)[0]
+            # without any open files necessary 
+            # https://stackoverflow.com/questions/30811918/saving-dictionary-of-numpy-arrays
+            np.save(os.path.join(args.output,name+'.npy'), r)
+                
+        
     else:
         print("'{}' is not recognized.Use 'train' or 'test'".format(args.command))
