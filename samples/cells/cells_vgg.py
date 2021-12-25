a63e2a64edad28144d86d026721ba411da2cc657
Vincent Arrigoni <varrigoni382@hotmail.fr>
2021-12-25 22:01:21 +0100

    submit syntax

--- samples/cells/cells_vgg.py
+++ samples/cells/cells_vgg.py
@@ -56,6 +56,9 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 #  Configurations
 ############################################################
 
+# Results directory
+# Save submission files here
+RESULTS_DIR = os.path.join(ROOT_DIR, "results/cells/")
 
 class CellConfig(Config):
     """Configuration for training on the toy  dataset.
@@ -85,7 +88,7 @@ class InferenceConfig(Config):
     IMAGES_PER_GPU = 1
     NAME = 'cell'
     # https://github.com/matterport/Mask_RCNN/issues/709
-    NUM_CLASSES =  3 +1 
+    NUM_CLASSES =  3 + 1 
     # https://github.com/matterport/Mask_RCNN/issues/410
 
 
@@ -279,6 +282,111 @@ def test(model, image_path = None, video_path=None, savedfile=None, classname = 
         pass
     print("Saved to ", os.getcwd() , file_name)
     
+############################################################
+#  Detection
+############################################################
+
+def detect(model, dataset_dir, subset):
+    """Run detection on images in the given directory."""
+    print("Running on {}".format(dataset_dir))
+
+    # Create directory
+    if not os.path.exists(RESULTS_DIR):
+        os.makedirs(RESULTS_DIR)
+    submit_dir = "submit_{:%Y%m%dT%H%M%S}".format(datetime.datetime.now())
+    submit_dir = os.path.join(RESULTS_DIR, submit_dir)
+    os.makedirs(submit_dir)
+
+    # Read dataset
+    dataset = NucleusDataset()
+    dataset.load_nucleus(dataset_dir, subset)
+    dataset.prepare()
+    # Load over images
+    submission = []
+    for image_id in dataset.image_ids:
+        # Load image and run detection
+        image = dataset.load_image(image_id)
+        # Detect objects
+        r = model.detect([image], verbose=0)[0]
+        # Encode image to RLE. Returns a string of multiple lines
+        source_id = dataset.image_info[image_id]["id"]
+        rle = mask_to_rle(source_id, r["masks"], r["scores"])
+        submission.append(rle)
+        # Save image with masks
+        visualize.display_instances(
+            image, r['rois'], r['masks'], r['class_ids'],
+            dataset.class_names, r['scores'],
+            show_bbox=False, show_mask=False,
+            title="Predictions")
+        plt.savefig("{}/{}.png".format(submit_dir, dataset.image_info[image_id]["id"]))
+
+    # Save to csv file
+    submission = "ImageId,EncodedPixels\n" + "\n".join(submission)
+    file_path = os.path.join(submit_dir, "submit.csv")
+    with open(file_path, "w") as f:
+        f.write(submission)
+    print("Saved to ", submit_dir)
+    
+    
+############################################################
+#  RLE Encoding
+############################################################
+
+def rle_encode(mask):
+    """Encodes a mask in Run Length Encoding (RLE).
+    Returns a string of space-separated values.
+    """
+    assert mask.ndim == 2, "Mask must be of shape [Height, Width]"
+    # Flatten it column wise
+    m = mask.T.flatten()
+    # Compute gradient. Equals 1 or -1 at transition points
+    g = np.diff(np.concatenate([[0], m, [0]]), n=1)
+    # 1-based indicies of transition points (where gradient != 0)
+    rle = np.where(g != 0)[0].reshape([-1, 2]) + 1
+    # Convert second index in each pair to lenth
+    rle[:, 1] = rle[:, 1] - rle[:, 0]
+    return " ".join(map(str, rle.flatten()))
+
+
+def rle_decode(rle, shape):
+    """Decodes an RLE encoded list of space separated
+    numbers and returns a binary mask."""
+    rle = list(map(int, rle.split()))
+    rle = np.array(rle, dtype=np.int32).reshape([-1, 2])
+    rle[:, 1] += rle[:, 0]
+    rle -= 1
+    mask = np.zeros([shape[0] * shape[1]], np.bool)
+    for s, e in rle:
+        assert 0 <= s < mask.shape[0]
+        assert 1 <= e <= mask.shape[0], "shape: {}  s {}  e {}".format(shape, s, e)
+        mask[s:e] = 1
+    # Reshape and transpose
+    mask = mask.reshape([shape[1], shape[0]]).T
+    return mask
+
+
+def mask_to_rle(image_id, mask, scores):
+    "Encodes instance masks to submission format."
+    assert mask.ndim == 3, "Mask must be [H, W, count]"
+    # If mask is empty, return line with image ID only
+    if mask.shape[-1] == 0:
+        return "{},".format(image_id)
+    # Remove mask overlaps
+    # Multiply each instance mask by its score order
+    # then take the maximum across the last dimension
+    order = np.argsort(scores)[::-1] + 1  # 1-based descending
+    mask = np.max(mask * np.reshape(order, [1, 1, -1]), -1)
+    # Loop over instance masks
+    lines = []
+    for o in order:
+        m = np.where(mask == o, 1, 0)
+        # Skip if empty
+        if m.sum() == 0.0:
+            continue
+        rle = rle_encode(m)
+        lines.append("{}, {}".format(image_id, rle))
+    return "\n".join(lines)
+    
 
 ############################################################
 #  Training
@@ -382,6 +490,7 @@ if __name__ == '__main__':
         # Train or evaluate
         print("Start Training !")
         train(model, epochs=int(args.epochs))
+        
     elif args.command == "test":
         # we test all models trained on the dataset in different stage
         print(os.getcwd())
@@ -392,24 +501,9 @@ if __name__ == '__main__':
                 model.load_weights(os.path.join(args.weights,filename),by_name=True)
                 savedfile_name = os.path.splitext(filename)[0] + ".jpg"
                 test(model, image_path=args.image,video_path=args.video, savedfile=savedfile_name, classname = args.classnames)
-    elif args.command == "submission":
-        filenames = os.listdir(args.image)
-        print("Load weights from {filename} ".format(filename=args.weights))
-        model.load_weights(os.path.join(args.weights),by_name=True)
-        for filename in filenames:
-            name, _ = filename.split('.')
-            image = skimage.io.imread(os.path.join(args.image, filename))
-            # https://github.com/matterport/Mask_RCNN/issues/1435
-            if image.ndim != 3:
-                image = skimage.color.gray2rgb(image)
-            # If has an alpha channel, remove it for consistency
-            if image.shape[-1] == 4:
-                image = image[..., :3]
-            # Detect objects
-            r = model.detect([image], verbose=1)[0]
-            # without any open files necessary 
-            # https://stackoverflow.com/questions/30811918/saving-dictionary-of-numpy-arrays
-            np.save(os.path.join(args.output,name+'.npy'), r)     
+                
+    elif args.command == "detect":
+        detect(model, args.dataset, args.subset)
         
     else:
         print("'{}' is not recognized.Use 'train' or 'test'".format(args.command))
